717cc79facc57fc5fa00a899a3efbfa507a5e6bb
Sarkar~Tech <bandahealimaree@gmail.com>
2024-12-05 15:16:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "TA5QmYpY#SkpdisxjMcELK4aJunYG6DDGcGLbhMIpT7OASZocT00",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
